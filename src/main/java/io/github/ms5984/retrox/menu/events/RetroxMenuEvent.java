0d9566b70d893c4394f6890f4b2d8614427dd3bb
Matt <58913953+ms5984@users.noreply.github.com>
2022-10-04 01:14:02 -0400

    Initial commit

---
+++ src/main/java/io/github/ms5984/retrox/menu/events/RetroxMenuEvent.java
@@ -0,0 +1,47 @@
+package io.github.ms5984.retrox.menu.events;
+/*
+ *  Copyright 2022 ms5984, Retrox
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+import org.bukkit.event.Event;
+import org.jetbrains.annotations.ApiStatus;
+
+/**
+ * Base class for RetroxMenu events.
+ *
+ * @since 1.0.0
+ * @author ms5984
+ */
+@ApiStatus.NonExtendable
+public abstract class RetroxMenuEvent extends Event {
+    /**
+     * Base class for RetroxMenu events that can be cancelled.
+     *
+     * @since 1.0.0
+     */
+    public abstract static class Cancellable extends RetroxMenuEvent implements org.bukkit.event.Cancellable {
+        private boolean cancelled;
+
+        @Override
+        public boolean isCancelled() {
+            return cancelled;
+        }
+
+        @Override
+        public void setCancelled(boolean cancel) {
+            cancelled = cancel;
+        }
+    }
+}
